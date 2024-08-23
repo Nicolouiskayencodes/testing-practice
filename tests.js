2102a1cddc8f010f130d15f5ec2a22d0904bdabd
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-08-22 22:38:29 -0700

    Add tests and write functions for reverse, calculator, and casesarCipher

--- tests.js
+++ tests.js
@@ -4,15 +4,52 @@ export function capitalize(string){
 }
 
 export function reverseString(string){
-
+ let split = string.split('');
+ let reverse = ''
+ for (let i=0; i<split.length; i++){
+  reverse += split[split.length-1-i]
+ }
+ return reverse
 }
 
 export let calculator = {
-
+  add(a,b){
+    return a+b;
+  },
+  subtract(a,b){
+    return a-b;
+  },
+  multiply(a,b){
+    return a*b;
+  },
+  divide(a,b){
+    return a/b;
+  }
 }
 
 export function caesarCipher(string) {
-
+  let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  let upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  let split = string.split('');
+  let newString = ''
+  split.forEach(character => {
+    if (lowerCase.includes(character)){
+      let index = lowerCase.indexOf(character) + 3;
+      if (index > lowerCase.length-1) {
+        index = index - lowerCase.length;
+      }
+      newString += lowerCase[index];
+    } else if (upperCase.includes(character)){
+      let index = upperCase.indexOf(character) + 3;
+      if (index > upperCase.length-1) {
+        index = index - upperCase.length;
+      }
+      newString += upperCase[index];
+  } else {
+    newString += character;
+  }
+});
+  return newString;
 }
 
 export function analyzeArray(array) {
--- tests.test.js
+++ tests.test.js
@@ -11,3 +11,63 @@ test('Capitalize 2', () =>{
 test('Capitalize 3', () =>{
   expect(capitalize('rIDICULOUS')).toBe('RIDICULOUS')
 });
+
+test('Reverse 1', () => {
+  expect(reverseString('hobbit')).toBe('tibboh')
+})
+
+test('Reverse 2', () => {
+  expect(reverseString('to isengard')).toBe('dragnesi ot')
+})
+
+test('Reverse 3', () => {
+  expect(reverseString('Mickey Mouse')).toBe('esuoM yekciM')
+})
+
+test('Add 1', () => {
+  expect(calculator.add(1,2)).toBe(3)
+})
+
+test('Add 2', () => {
+  expect(calculator.add(67, 13)).toBe(80)
+})
+
+test('Subtract 1', () => {
+  expect(calculator.subtract(4, 2)).toBe(2)
+})
+
+test('Subtract 2', () => {
+  expect(calculator.subtract(187, 50)).toBe(137)
+})
+
+test('Subtract Negative', () => {
+  expect(calculator.subtract(3, 7)).toBe(-4)
+})
+
+test('Multiply 1', () => {
+  expect(calculator.multiply(20, 2)).toBe(40)
+})
+
+test('Multiply Deciaml', () => {
+  expect(calculator.multiply(1.5, 3)).toBe(4.5)
+})
+
+test('Divide 1', () => {
+  expect(calculator.divide(20, 2)).toBe(10)
+})
+
+test('Divide Decimal', () => {
+  expect(calculator.divide(4.5, 3)).toBe(1.5)
+})
+
+test('Casesar Cipher 1', () => {
+  expect(caesarCipher('xyz', 3)).toBe('abc')
+})
+
+test('Casesar Cipher Case Sensitive', () => {
+  expect(caesarCipher('HeLLo', 3)).toBe('KhOOr')
+})
+
+test('Casesar Cipher Punctuation', () => {
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!')
+})
